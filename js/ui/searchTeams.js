e1435c7bb84aa5bdb1272069545bf2ebc3868aef
pdtunes <55880640+pdtunes@users.noreply.github.com>
2021-02-10 12:01:05 +0100

    update

--- js/ui/searchTeams.js
+++ js/ui/searchTeams.js
@@ -1,19 +1,19 @@
+import _ from "lodash";
 import { renderTeams } from "./renderTeams.js";
 
 export function searchTeams(teams) {
-    const search = document.querySelector(".search");
+  const search = document.querySelector(".search");
 
-    search.onkeyup = function (event) {
-        // console.log(event);
+ function doTheSearch(event) {
+    console.log(event.key);
+    const searchValue = event.target.value.trim().toLowerCase();
+    const filteredTeams = teams.filter(function (team) {
+      if (team.full_name.toLowerCase().startsWith(searchValue)) {
+        return true;
+      }
+    });
 
-        const searchValue = event.target.value.trim().toLowerCase();
-
-        const filteredTeams = teams.filter(function (team) {
-            if (team.full_name.toLowerCase().startsWith(searchValue)) {
-                return true;
-            }
-        });
-
-        renderTeams(filteredTeams);
-    };
+    renderTeams(filteredTeams);
+  };
+  search.onkeyup = _.debounce(doTheSearch, 600, {leading: true, maxWait: 1000})
 }
